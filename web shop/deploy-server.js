a9ba2a0aabf9a1d82a3b58aea51642cf9f7d2ce5
marcuseriksson2019 <50358847+marcuseriksson2019@users.noreply.github.com>
2019-08-30 18:59:03 +0200

    Delete deploy-server.js

--- web shop/deploy-server.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var app = express();
-app.use("/node_modules",
-express.static("/usr/src/health shop/node_modules"));
-app.use("/", express.static("/usr/src/health shop/app"));
-app.listen(3000, function () {
-console.log("HTTP Server running on port 3000");
-});
